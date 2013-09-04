3213af65bec85745b3eb2d02d5b0c2248899be0a
elektromin <elektromin@hotmail.com>
2013-09-04 13:21:23 +0200

    lagt till mylib

--- gittest2/Program.cs
+++ gittest2/Program.cs
@@ -6,11 +6,16 @@ using System.Threading.Tasks;
 
 namespace gittest2
 {
+    using mylib;
+
     class Program
     {
         static void Main(string[] args)
         {
             System.Console.WriteLine("Hello git");
+            var owner = new Person() { Name = "Musse" };
+            var pet = new Pet() { Owner = owner, Name = "Pluto" };
+
         }
     }
 }
---
+++ mylib/Pet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mylib
+{
+    public class Pet
+    {
+        public Person Owner { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+}
